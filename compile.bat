2026dc12f7b3b583088eb26fb65bcb50a01e348b
NalyAndriam <153374026+NalyAndriam@users.noreply.github.com>
2024-05-09 19:03:27 +0300

    edit compile.bat

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
 @echo off
 javac -cp "C:\Users\nalya\OneDrive\Bureau\IT\jar\servlet.jar" -d bin src\controller\*.java
-jar cvf Framework.jar -C bin .
+jar cvf "C:\Users\nalya\OneDrive\Bureau\IT\S3S4\S4\MrNaina\Framework.jar" -C bin .
