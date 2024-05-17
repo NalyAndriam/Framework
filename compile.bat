059f78541ebbd7c4cb801b4b6c69b948abde36a1
NalyAndriam <153374026+NalyAndriam@users.noreply.github.com>
2024-05-17 11:50:40 +0300

    sprint1 sending

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
 @echo off
-javac -cp "C:\Users\nalya\OneDrive\Bureau\IT\jar\servlet.jar" -d bin src\controller\*.java
+javac -cp "C:\Program Files\Apache Software Foundation\Tomcat 10.1\lib\servlet-api.jar" -d bin src\controller\*.java src\util\*.java
 jar cvf "C:\Users\nalya\OneDrive\Bureau\IT\S3S4\S4\MrNaina\Framework.jar" -C bin .
--- src/controller/FrontController.java
+++ src/controller/FrontController.java
@@ -1,28 +1,57 @@
 package controller;
 
 import java.io.IOException;
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.rmi.ServerException;
+import java.util.Vector;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import util.Utilitaire;
 
 public class FrontController extends HttpServlet {
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
 
-        String url = request.getRequestURL().toString();
-        System.out.println("Tonga eto amin'ny " + url);
+    String packagePath;
+    boolean isChecked;
+    Vector<String> controllers;
+
+    public void init(){
+        try{
+            controllers= new Utilitaire().getListController(this.getInitParameter("package"), AnnotationController.class);
+            isChecked= true;
+        }
+        catch (Exception e){
+            throw new RuntimeException();
+        }
+    }
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException, ClassNotFoundException {
+        PrintWriter out = response.getWriter();
+        // String url = request.getRequestURL().toString();
+        // out.println("Tonga eto amin'ny " + url);
+        if (!isChecked){
+            init();
+        }
+        else{
+            for (int i=0; i<controllers.size(); i++){
+                out.println(controllers.get(i));
+            }
+        }
     }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, ClassNotFoundException{
         processRequest(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
+            throws ServletException, IOException, ClassNotFoundException {
         processRequest(request, response);
     }
 }
