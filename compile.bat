d384915b04318fa293d86c7e05cd18ddd2d91ed1
NalyAndriam <153374026+NalyAndriam@users.noreply.github.com>
2024-06-07 09:36:52 +0300

    sprint4

--- compile.bat
+++ compile.bat
@@ -1,3 +1,28 @@
 @echo off
-javac -cp "C:\Program Files\Apache Software Foundation\Tomcat 10.1\lib\servlet-api.jar" -d bin src\controller\*.java src\util\*.java src\annotation\*.java
-jar cvf "C:\Users\nalya\OneDrive\Bureau\IT\S3S4\S4\MrNaina\Framework.jar" -C bin .
+setlocal
+:: Define your project directories
+set PROJECT_DIR=%~dp0
+set SRC_DIR=%PROJECT_DIR%src
+set BIN_DIR=%PROJECT_DIR%bin
+set LIB_DIR=%PROJECT_DIR%lib
+set TMP_DIR=%PROJECT_DIR%tmp
+:: Create bin and tmp directories if they don't exist
+if not exist "%BIN_DIR%" mkdir "%BIN_DIR%"
+if not exist "%TMP_DIR%" mkdir "%TMP_DIR%"
+:: Copy only .java files from src to tmp
+for /R "%SRC_DIR%" %%f in (*.java) do (
+    xcopy "%%f" "%TMP_DIR%" /I
+)
+:: Compile the code
+cd "%TMP_DIR%"
+@REM javac -cp "%BIN_DIR%;%LIB_DIR%\servlet-api.jar" -d "%BIN_DIR%" "%SRC_DIR%\annotation\*.java" "%SRC_DIR%\util\*.java" "%SRC_DIR%\mg\itu\prom16\*.java" 
+javac -d "%BIN_DIR%" -cp "%LIB_DIR%/*" "*.java"
+:: Create the jar
+cd "%BIN_DIR%"
+jar cvf "framework.jar" *
+move "framework.jar" "%PROJECT_DIR%"
+:: Clean up tmp directory
+rd /S /Q "%TMP_DIR%"    
+endlocal
+
+
---
+++ src/util/ModelAndView.java
@@ -0,0 +1,46 @@
+package util;
+
+import java.util.HashMap;
+
+public class ModelAndView {
+    String url ;
+    HashMap<String ,Object> data;
+    /**
+     * 
+     */
+    public ModelAndView() {
+        data = new HashMap<String, Object>();
+    }
+    /**
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+    /**
+     * @param url the url to set
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+    /**
+     * @return the data
+     */
+    public HashMap<String, Object> getData() {
+        return data;
+    }
+    /**
+     * @param data the data to set
+     */
+    public void setData(HashMap<String, Object> data) {
+        this.data = data;
+    } 
+
+    public void addObject(String variableName,Object value){
+        if(data == null){
+             data = new HashMap<String ,Object>();
+        }
+        this.data.put(variableName, value);
+    }
+    
+}
